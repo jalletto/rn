537667e0eb75773e2b5c8767553ee53e712244d4
jalletto <joshalletto@gmail.com>
2023-09-29 19:16:16 -0500

    update readme and add gifs.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # RN - Rename Utility Built With [Tview](https://github.com/rivo/tview)
 
-Navigate directories with an interactive tree view. Rename and delete files. Batch rename files.
+I built this to help me rename large amounts of files on my server. It allows you to:
 
+- Rename multiple files at once with a find and replace feature (regex coming soon)
+- Navigate directories with an interactive tree view
+- Rename and delete directories and single files using a form
 
-![batch rename](./imgs/batch.png)
+![batch rename](./imgs/batch.gif)
 
-![tree rename](./imgs/tree.png)
+![tree rename](./imgs/tree.gif)
--- main.go
+++ main.go
@@ -71,30 +71,3 @@ func main() {
 	}
 
 }
-
-// TODO
-// I want to be able to press 'm' and get an option to move the file.
-// - I can type in a path
-// - I can choose from a list of common paths
-// - I can create a common path to add to the list
-// Add append and prepend all in Batch.
-// I want to be able to enter RegEx to select
-// I want to be able to Batch delete based on selections
-
-// Found Bugs
-
-// DONE
-// Create a file class to hold all file data. Then update the node reference to use the file class.
-// I want to be able to rename the dirs and files from the tree
-// I want to see the dirs in the my current root.
-// I want to be able to see the files in those dirs
-// I want to be able to press 'd' to delete a file
-// I want to be able to rename all the files in a directory at once.
-// - If I click on a dir in the tree I am taken to new screen where I can see a list of all files
-// - There is a form where I can set a find pattern and a string to replace it with.
-// - I can click a button to batch rename all the files
-
-// Bugs Done
-// If you rename a directory and then try to rename a file in that directory you get an error.
-// - Cause: we are relying on the node's reference for the file path. This get's set when we first generate the tree but is not updated after that.
-// - Possible solution: If we change the reference to a struct that contains all the file data, we can check if we are renaming a dir or not. If we are, then we can get the nodes children and recursively update all child nodes. We could also just completely regenerate the tree. Not sure which is more of a pain in the ass.
